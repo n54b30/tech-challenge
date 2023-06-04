a046139eef0ac2156585dbe25553947ee193ab6e
Alex_Vasile <alexandru.vasile@rezatec.com>
2023-06-04 17:39:41 +0100

    remove couple of dupes

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
-# Set GCP project and region variables
-variable "project" {
-  type    = string
-  default = "nucleus-380110"
+# Set number of nodes
+
+variable "gke_num_nodes" {
+  default     = 2
+  description = "number of gke nodes"
 }
 
-variable "region" {
-  type    = string
-  default = "europe-west2"
-}
 
 # Configure the GCP provider
 provider "google" {
@@ -16,11 +13,27 @@ provider "google" {
   region      = var.region
 }
 
-# Provision a GKE cluster
-resource "google_container_cluster" "cluster" {
-  name               = "cluster123"
+# GKE cluster
+resource "google_container_cluster" "primary" {
+  name     = "${var.project}-gke"
+  location = var.region
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  network    = google_compute_network.vpc.name
+  subnetwork = google_compute_subnetwork.subnet.name
+}
+
+# Provision a Managed Node pool
+resource "google_container_node_pool" "primary_nodes" {
+  name               = google_container_cluster.primary.name
   location           = var.region
-  initial_node_count = 3
+  cluster            = google_container_cluster.primary.name
+  node_count = var.gke_num_nodes
 
   node_config {
     oauth_scopes = [
@@ -29,15 +42,25 @@ resource "google_container_cluster" "cluster" {
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring"
     ]
+
+    labels = {
+      env = var.project
+    }
+
+    machine_type = "n1-standard-1"
+    tags         = ["gke-node", "${var.project}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
   }
 }
 
 # Configure kubectl to connect to the GKE cluster
 resource "null_resource" "configure_kubectl" {
   provisioner "local-exec" {
-    command = "gcloud container clusters get-credentials ${google_container_cluster.cluster.name} --region ${var.region} --project ${var.project}"
+    command = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --region ${var.region} --project ${var.project}"
   }
-  depends_on = [google_container_cluster.cluster]
+  depends_on = [google_container_node_pool.primary_nodes]
 }
 
 # Apply a Helm chart to the GKE cluster
